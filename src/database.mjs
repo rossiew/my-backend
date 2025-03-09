055b82e73581b8c68fe0fe2d2bb53d40601de890
Rose Tewm <temirbajroza@gmail.com>
2025-03-09 14:40:08 +0500

    Render DB үшін pool орнаттым

--- src/database.mjs
+++ src/database.mjs
@@ -1,15 +1,27 @@
-import pg from "pg"
-//pg- js/postgre байланыстыру 
+// import pg from "pg"
+// //pg- js/postgre байланыстыру 
 
+
+// const pool = new pg.Pool({
+//     user: "postgres",
+//     host: "localhost",
+//     password: "roza2006",
+//     port: 5432,
+//     database: "postgres" //database/ қай жерде турғанын жазамыз 
+// })
+
+
+// export default pool;
+
+
+import pg from "pg";
 
 const pool = new pg.Pool({
-    user: "postgres",
-    host: "localhost",
-    password: "roza2006",
-    port: 5432,
-    database: "postgres" //database/ қай жерде турғанын жазамыз 
-})
-
+  connectionString: "postgresql://rossiew:At02m9D2Kts4w0bo5YMpBRnf73uOZVzd@dpg-cv6m6lqj1k6c73e64nlg-a/mydb_kf9m",
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 export default pool;
 
